9154b39cd6e271dd62edeabf8952c33bccd6a544
amendez <amendez@ipcomnetwork.com>
2016-02-11 10:46:24 -0500

    cambios recientes

--- polls/views.py
+++ polls/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from django.http import HttpResponse
+
+def index(request):
+    return HttpResponse("hello world -> Bienvenido al modulo de Encuestas")
+
+def detail(request, question_id):
+    return HttpResponse("Pregunta %s." % question_id)
+
+def results(request, question_id):
+    response = "Resultado %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("Su voto para la pregunta %s." % question_id)
